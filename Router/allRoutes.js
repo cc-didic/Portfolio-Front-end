cd6c3114bea0b8d8d6178cb04676866699365067
cc-didic <cc.cedric.caron@gmail.com>
2026-01-20 13:47:47 +0100

    reorganization

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -8,8 +8,8 @@ import Route from "./Route.js";
 // Définir ici les routes
 export const allRoutes = [
     new Route("/", "Acceuil", "/pages/home.html"),
-    new Route("/signin", "Connexion", "/pages/signin.html"),
-    new Route("/create-project", "Ajouter un projet", "/pages/createProject.html"),
+    new Route("/signin", "Connexion", "/pages/auth/signin.html"),
+    new Route("/create-project", "Ajouter un projet", "/pages/auth/createProject.html"),
 ];
 
 // Le titre s'affiche comme ceci : Route.titre - websitename
