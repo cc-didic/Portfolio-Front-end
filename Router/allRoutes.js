6a4c1993c2f3805caffc2ea3565b5ba19635cd11
cc-didic <cc.cedric.caron@gmail.com>
2026-01-21 11:08:08 +0100

    Refactoring and adding createProject.js

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -18,8 +18,8 @@ import Route from "./Route.js";
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html", []),
     new Route("/home", "Accueil", "/pages/home.html", []),
-    new Route("/signin", "Connexion", "/pages/auth/signin.html", ["disconnected"], "/js/auth/signin.js"),
-    new Route("/create-project", "Ajouter un projet", "/pages/auth/createProject.html", ["admin"]),
+    new Route("/signin", "Connexion", "/pages/auth/signin.html", ["disconnected"], "/assets/js/auth/signin.js"),
+    new Route("/create-project", "Ajouter un projet", "/pages/auth/createProject.html", ["admin"], "/assets/js/createProject.js"),
 ];
 
 // Le titre s'affiche comme ceci : Route.titre - websitename
