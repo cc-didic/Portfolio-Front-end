729b505578517a8c0f5f0d8257ec39cb91b7f573
cc-didic <cc.cedric.caron@gmail.com>
2026-01-23 13:13:53 +0100

    Added function to send project creation to the database

--- assets/js/createProject.js
+++ assets/js/createProject.js
@@ -1,1 +1,40 @@
-alert("coucou");
+const createProjectBtn = document.getElementById("create-project-btn");
+
+createProjectBtn.addEventListener("click", createProject);
+
+function createProject(){
+    let myHeaders = new Headers();
+    myHeaders.append("X-AUTH-TOKEN", getToken(), "Content-Type", "application/json");
+
+    const signin = {
+        title: document.getElementById("TitleInput").value,
+        description: document.getElementById("DescriptiondInput").value,
+        image: document.getElementById("ImageInput").value,
+        github_url: document.getElementById("GithubInput").value,
+        live_url: document.getElementById("LiveInput").value
+    };
+
+	let requestOptions = {
+		method : 'POST',
+		headers : myHeaders,
+        body: JSON.stringify(signin),
+        redirect: 'follow',
+        credentials: "include"
+	};
+
+	// Envoi une requete au serveur
+    console.log(apiUrl+"project");
+	fetch(apiUrl+"project", requestOptions)
+	.then(response =>{
+		if(!response.ok){
+            throw new Error("Données invalides");
+		}
+        return response.json();
+	})
+	.then(result => {
+		console.log(result);
+	})
+	.catch(error => {
+		console.log('error', error);
+	});
+}
